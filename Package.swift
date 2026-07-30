09a6e3d5e4f9f89b71f159a32e14d77e477f796b
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-07-30 23:46:53 +0100

    updated dependencies

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrueFaceLiveness",
-            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v1.0.0/TrueFaceLiveness.xcframework.zip",
+            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v1.0.1/TrueFaceLiveness.xcframework.zip",
             checksum: "76432d502d265db6d9c60f90f8d3cb6e7f85a7926d4492e71444d834c88ac618"
         )
     ]
