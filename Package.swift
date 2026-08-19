a4c98c81815e33bb0aa8dd360bb55e69ee2857bd
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-08-19 10:30:01 +0100

    upgraded version

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrueFaceLiveness",
-            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.2/TrueFaceLiveness.xcframework.zip",
-            checksum: "223a8377a9793d22de75490c31f28be1c3fe0e43d3a57424eb7b2b933b9e35bd"
+            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.3/TrueFaceLiveness.xcframework.zip",
+            checksum: "e04a7893b1e6882ce8e731254cb6f1d7f551f9c7fd18d469a8d7a3d240568fc5"
         )
     ]
 )
