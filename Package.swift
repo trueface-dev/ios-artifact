f3e35e78bbd6ffc2acc1b3170c04cd439efcf030
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-08-20 11:22:17 +0100

    Update binary target and checksum for v0.2.7 (with Info.plist)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrueFaceLiveness",
-            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.6/TrueFaceLiveness.xcframework.zip",
-            checksum: "058e6f95d7a11cd74fa7087ca8f8151cb829b0d77985d738b550d3250e4a5e12"
+            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.7/TrueFaceLiveness.xcframework.zip",
+            checksum: "3b83ef04ee9f15688bacc044fad76a351e857019aa5946b94a16f8282ea6705e"
         )
     ]
 )
