814434f72a3c444640b2b94634b138fb1dd447e3
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2026-08-21 15:03:32 +0100

    Update binary target and checksum for v0.2.8 (eyes-open validation)

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TrueFaceLiveness",
-            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.7/TrueFaceLiveness.xcframework.zip",
-            checksum: "3b83ef04ee9f15688bacc044fad76a351e857019aa5946b94a16f8282ea6705e"
+            url: "https://github.com/trueface-dev/ios-artifact/releases/download/v0.2.8/TrueFaceLiveness.xcframework.zip",
+            checksum: "a25c28e69c696bdb0e91ad9443b0d1f2a02152c226eb7b3b108c812cd87ab6cc"
         )
     ]
 )
